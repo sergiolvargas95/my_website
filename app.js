cd6dcfecd0bc209f249d4c97041ea9c06a48577f
sergiolvargas95 <65960993+sergiolvargas95@users.noreply.github.com>
2021-02-02 19:41:21 -0500

    cambios realizados al menu hamburguesa

--- app.js
+++
@@ -1,8 +0,0 @@
-const btn = document.querySelector('#menu-btn')
-const menu = document.querySelector('#sidemenu')
-btn.addEventListener('click', e =>{
-    menu.classList.toggle("menu-expanded")
-    menu.classList.toggle("menu-collased")
-    document.querySelector('body').classList.toggle('body-expanded')
-})
-
